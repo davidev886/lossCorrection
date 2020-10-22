642e4043374f853c287baa6706336fcd25fe3848
Davide Vodola <davide.vodola@gmail.com>
2020-10-22 16:09:03 +0200

    Added comments

--- utils/utils_qnd.py
+++ utils/utils_qnd.py
@@ -2,7 +2,11 @@ import numpy as np
 from itertools import product
 
 def pick_qnd_error(p_qnd):
-
+    """
+    generate a depolarizing channel with probability p_qnd
+    returns a string with "Eq Ea" with a Pauli operator on the data qubit and the ancilla
+    representing the error
+    """
     random_toss_qnd = np.random.binomial(1, p_qnd, 1)
     if not (sum(random_toss_qnd)):
         #no qnd_error

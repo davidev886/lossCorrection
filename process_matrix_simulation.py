8af504327c8f40cead2d5662cb8ba5a85ed5d1cc
Davide Vodola <davide.vodola@unibo.it>
2020-11-05 00:27:11 +0100

    Simulate process matrix

---
+++ process_matrix_simulation.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+    
+import numpy as np
+import qutip as qu
+
+from utils.utils_qnd import pick_qnd_error
+
+from utils.p_operators import *
+
+from random import randint
+seme = randint(0,100)
+
+seme=5
+np.random.seed(seme)
+
+print("\nseed:", seme)
+

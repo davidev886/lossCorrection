83c169bd7bc15c846f439a5070253fc88e31be3b
Davide <davide.vodola@unibo.it>
2020-11-11 14:50:38 +0100

    function for choi

--- utils/choi_repr.py
+++ utils/choi_repr.py
@@ -28,31 +28,43 @@ def proj(ket, bra, dimH = 2):
     return ket_s * bra_s
 
 
-dimHa = 2
-dimHq = 3
-dimTot = dimHa * dimHq
-GammaState = sum([qu.tensor(qu.basis(dimHa, ja), qu.basis(dimHq, jq), qu.basis(dimHa, ja), qu.basis(dimHq, jq)) 
-                        for ja,jq in product(range(dimHa), range(dimHq))])
-              
-rhoGamma = GammaState * GammaState.dag()        
-
-A0 = proj(1, 1, dimHq) + proj(0, 0, dimHq)
-A1 = proj(2, 2, dimHq)
-
-U = qu.tensor(qu.qeye(dimHa), A0) + qu.tensor(qu.sigmax(), A1)
-
-choiState = qu.tensor(qu.qeye(dimHa), qu.qeye(dimHq), U) * rhoGamma * qu.tensor(qu.qeye(dimHa), qu.qeye(dimHq), U.dag())
-#np.savetxt("choiState.dat", choiState.full().real)
+def apply_choi(rho, choiState):
+    dimTot = stateTry.shape[0]
+    if rho.type == "ket":
+        rho = (rho * rho.dag()).unit()
 
 
-stateQubit = (np.random.random() * qu.basis(dimHq, 0) + np.random.random() * qu.basis(dimHq, 1)).unit()
-
-stateTry = qu.tensor(qu.basis(dimHa, 0), stateQubit)
-stateTry = stateTry * stateTry.dag()
-print(stateTry)
-finalState = sum([stateTry[i,j] * choiState[dimTot*i : dimTot*i + dimTot, dimTot*j: dimTot*j + dimTot] 
-                  for i,j in product(range(dimTot), range(dimTot))])
-
-finalState = qu.Qobj(inpt=finalState, dims=stateTry.dims)
+    finalState = sum([rho[i,j] * choiState[dimTot*i : dimTot*i + dimTot, dimTot*j: dimTot*j + dimTot] 
+                   for i,j in product(range(dimTot), range(dimTot))])    
     
-print(finalState - stateTry)
+    finalState = qu.Qobj(inpt=finalState, dims=rho.dims)
+    return finalState
+
+
+if __name__ == "__main__":
+    dimHa = 2
+    dimHq = 3
+    dimTot = dimHa * dimHq
+    GammaState = sum([qu.tensor(qu.basis(dimHa, ja), qu.basis(dimHq, jq), qu.basis(dimHa, ja), qu.basis(dimHq, jq)) 
+                            for ja,jq in product(range(dimHa), range(dimHq))])
+              
+    rhoGamma = GammaState * GammaState.dag()        
+
+    A0 = proj(1, 1, dimHq) + proj(0, 0, dimHq)
+    A1 = proj(2, 2, dimHq)
+
+    U = qu.tensor(qu.qeye(dimHa), A0) + qu.tensor(qu.sigmax(), A1)
+
+    choiState = qu.tensor(qu.qeye(dimHa), qu.qeye(dimHq), U) * rhoGamma * qu.tensor(qu.qeye(dimHa), qu.qeye(dimHq), U.dag())
+
+    stateQubit = (np.random.random() * qu.basis(dimHq, 0) + np.random.random() * qu.basis(dimHq, 1)).unit()
+
+    stateTry_ket = qu.tensor(qu.basis(dimHa, 0), stateQubit)
+    stateTry = stateTry_ket * stateTry_ket.dag()
+
+    finalState = apply_choi(stateTry, choiState)
+
+    print(finalState)
+
+    print(apply_choi(stateTry_ket, choiState))
+

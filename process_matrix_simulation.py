e8d2e9f38337a8d8c0170182e607b283ba9057f9
Davide <davide.vodola@unibo.it>
2020-11-12 17:55:04 +0100

    Add first for on losses

--- process_matrix_simulation.py
+++ process_matrix_simulation.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import qutip as qu
 
-from utils.utils_qnd import pick_qnd_error
+from utils.qnd_error_gen import pick_qnd_error
 
-from utils.p_operators import *
+from utils.p_operators_qutrit import *
 
 from random import randint
 seme = randint(0,100)
@@ -15,3 +15,17 @@ np.random.seed(seme)
 
 print("\nseed:", seme)
 
+a = np.random.random()  + np.random.random() * 1j
+b = np.random.random()  + np.random.random() * 1j
+
+psiL = (a * ZeroL + b * OneL).unit()
+
+#w_0 = psiL.ptrace(kept_qubits)
+#w = (qu.tensor([qu.fock_dm(2,0)] * len(losses) + [w_0])).unit()
+phi = 0.1 * np.pi
+
+R_loss_list = Rloss(phi)
+
+for data_q in [0]:
+    #apply Rloss with an angle phi
+    print(R_loss_list[data_q] * psiL)

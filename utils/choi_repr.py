6b97375c99e0c0be7ee66632575493843bf6f674
Davide Vodola <davide.vodola@unibo.it>
2020-12-03 09:50:47 +0100

    Clean

--- utils/choi_repr.py
+++ utils/choi_repr.py
@@ -26,7 +26,7 @@ def apply_choi(rho, choiState):
     return finalState
 
 # for plotting experimental choi operators
-if __name__ == "__main__":
+if 0: #__name__ == "__main__":
     import matplotlib.pyplot as plt
     from matplotlib import colors
     from itertools import product
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     
 
 # for plotting ideal choi operators
-if 0:
+if __name__ == "__main__":
     dimHa = 2
     dimHq = 3
     dimTot = dimHa * dimHq
@@ -96,8 +96,23 @@ if 0:
         A0 = proj(1, 1, dimHq) + np.cos(phi/2) * proj(0, 0, dimHq) + np.sin(phi/2) * proj(0, 2, dimHq)
         A1 =  - np.sin(phi/2) * proj(2, 0, dimHq) + np.cos(phi/2) * proj(2, 2, dimHq)
 
-        U = qu.tensor(qu.qeye(dimHa), A0) + qu.tensor(qu.sigmax(), A1)
-
+#        U = qu.tensor(qu.qeye(dimHa), A0) + qu.tensor(qu.sigmax(), A1)
+        rot =  [ [np.cos(phi/2), 0, np.sin(phi/2)], 
+                  [0, 1, 0],  
+                  [-np.sin(phi/2), 0, np.cos(phi/2)]
+                ]
+        Rloss =  qu.tensor(qu.qeye(dimHa), qu.Qobj(rot))
+        
+        msGate = qu.tensor(qu.qeye(dimHa), proj(2, 2, 3)) +  qu.tensor(qu.sigmax(), qu.Qobj([[0, 1, 0], [1, 0, 0], [0, 0, 0]]))
+        RxA = qu.tensor(qu.sigmax(), qu.qeye(dimHq)) # bit flip ancilla
+        RxQ = qu.tensor(qu.qeye(dimHa), qu.Qobj([[0, 1, 0], [1, 0, 0], [0, 0, 0]]) + qu.Qobj([[0, 0, 0], [0, 0, 0], [0, 0, 1]]))  # bit flip qutrit       
+        print(RxA.dims)
+        print(RxQ.dims)        
+        print(msGate.dims)                
+        print(Rloss.dims)
+        print(RxQ)
+        U = RxA * RxQ * msGate * Rloss
+        
         choiState = qu.tensor(qu.qeye(dimHa), qu.qeye(dimHq), U) * rhoGamma * qu.tensor(qu.qeye(dimHa), qu.qeye(dimHq), U.dag())
 
         import matplotlib.pyplot as plt
@@ -107,7 +122,7 @@ if 0:
         base_S = [(x,y) for x,y in product(range(2), range(3))]
         label_axis =[ "".join([str(_) for _ in bR])+ "," + "".join([str(_) for _ in bS]) for (bR, bS) in product(base_S, base_S) ]
         CC = choiState.full() / 6
-        np.savetxt(f"choiFinal_ideal_{phi_tilde:1.2f}.dat", CC, delimiter=',')
+
 
         for A in [choiState.full().real / 6]: #, choiState.full().imag / 6] :   
             fig, ax = plt.subplots()
@@ -135,8 +150,8 @@ if 0:
                 ax.axhline(y=6*j - 0.5,color='lightgrey', linewidth=1, linestyle='-')
                 ax.axvline(x=6*j - 0.5,color='lightgrey', linewidth=1, linestyle='-')
             #ax.yaxis.grid(True, which='major')
-    #        plt.show()
+            plt.show()
     
     
-    
-        plt.savefig(f"choiFinal_ideal_{phi_tilde}.pdf", bbox_inches='tight')
+#         np.savetxt(f"choiFinal_ideal_{phi_tilde:1.2f}.dat", CC, delimiter=',')   
+    #    plt.savefig(f"choiFinal_ideal_{phi_tilde}.pdf", bbox_inches='tight')

51f102326475f124075213878a7acf866475a348
Davide Vodola <davide.vodola@unibo.it>
2020-12-05 19:08:43 +0100

    script

--- start_prob.sh
+++ start_prob.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-OMP_NUM_THREADS=2
+OMP_NUM_THREADS=1
     
 eps=0.00
 for state in 0 1 2
 do
-for c in 0.05  0.1  0.15  0.2  0.25  0.3  0.35  0.4  0.45  0.5  0.55  0.6  0.65  0.7  0.75  0.8  0.85  0.9  0.95  1.
+for c in 0.05  0.1  0.15  0.2  0.25  0.3  0.35  0.4  0.45  0.5  0.55  0.6  0.65  0.7  0.75  0.8  0.85  0.9  0.95
 do
         python process_matrix_simulation_all.py --logical_state ${state}  --phi_tilde ${c}  --epsilon_choi ${eps} 2>&1 >& 0_log_${c}_${eps}_${state}.log & 
 done

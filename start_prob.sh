e542cb77e15c5369e5ce7446afc847d47ec6e78f
Davide Vodola <davide.vodola@unibo.it>
2021-01-11 10:40:05 +0100

    Create folder for data

--- start_prob.sh
+++ start_prob.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export OMP_NUM_THREADS=1
     
-eps=0.10
+eps=0.90
+chi=0.0
+mkdir -p  $(printf "chi_%.01e_eps_%1.3f" $chi $eps )
+wait
+
 for state in 0 2 4 
 do
 for c in 0.05  0.1  0.15  0.2  0.25  0.3  0.35  0.4  0.45  0.5  0.55  0.6  0.65  0.7  0.75  0.8  0.85  0.9  0.95

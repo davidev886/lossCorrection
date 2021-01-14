15cc81291e66c3274c1f5551b5120ee6e22aa390
Davide Vodola <davide.vodola@unibo.it>
2021-01-14 10:17:36 +0100

    Script for averaging over all stab measurements

---
+++ over_all_stab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export OMP_NUM_THREADS=1
+
+eps=0.9
+chi=1.0e-3
+mkdir -p  $(printf "chi_%.01e_eps_%1.3f_over_stab" $chi $eps )
+wait
+
+for state in  0 4
+do
+for c in 0.05  0.1  0.15  0.2  0.25  0.3  0.35  0.4  0.45  0.5  0.55  0.6  0.65  0.7  0.75  0.8  0.85  0.9  0.95
+do
+        python simulation_all_over_stabilizers.py --logical_state ${state}  --phi_tilde ${c}  --epsilon_choi ${eps} --chi_threshold ${chi} 2>&1 >& 0_log_${c}_${eps}_${state}.log &
+done
+done
+
+

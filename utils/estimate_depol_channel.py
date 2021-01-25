13ff11da3c4f759046ea520ae917a11f134fab4b
Davide Vodola <davide.vodola@unibo.it>
2021-01-25 23:24:11 +0100

    Compute distance between chois

--- utils/estimate_depol_channel.py
+++ utils/estimate_depol_channel.py
@@ -26,7 +26,32 @@ def proj(ket, bra, dimH = 2):
            
     return ket_s * bra_s
 
+def depolarization_channel(p_dep):
+# Define Omega state for computing Choi matrix 
+# omega = [00,01,02,10,11,12] * [00,01,02,10,11,12] ancilla first
+    omega = sum([qu.tensor(qu.basis(2, i), qu.basis(3, j), 
+                 qu.basis(2, i), qu.basis(3, j)) 
+                for i, j in product(range(2), range(3))])
 
+    omega_state = omega * omega.dag()
+
+    dimHtot = 2 * 3
+    
+    choi_depolarizing = (p_dep * qu.tensor(qu.qeye(2), qu.qeye(3), qu.qeye(2) , qu.qeye(3))/dimHtot 
+                            + (1 - p_dep) * omega_state)
+
+    vals, vecs = eigh(choi_depolarizing.data.todense())
+
+    kraus_matrix = [np.sqrt(vals[j]) * vecs[:, j].reshape((dimHtot, dimHtot)).T for j in range(len(vecs))]
+
+
+    system_length = len(choi_depolarizing.dims[0])//2
+    dim_kraus = [choi_depolarizing.dims[0][-system_length:], choi_depolarizing.dims[0][-system_length:]]   
+    kraus_ops = [qu.Qobj(inpt=op, dims=dim_kraus) for op in kraus_matrix]
+
+    channel_dep = sum([qu.spre(op) * qu.spost(op.dag()) for op in kraus_ops])
+    return channel_dep
+    
 
 if __name__ == "__main__":
     np.set_printoptions(precision = 4, suppress = True, linewidth=100000)
@@ -40,70 +65,41 @@ if __name__ == "__main__":
 #    A1 = |2><2|
     A1 = proj(2, 2, dimH = 3)
 
-    
-#   U_ideal = 1_a  A0 + X_a  A1
-    U_ideal = qu.tensor(qu.qeye(2), A0) + qu.tensor(qu.sigmax(), A1)
-#   U_ideal = 
+    id_a = qu.qeye(2)
+    X_a = qu.sigmax()
+    id_all = qu.tensor(qu.qeye(2), qu.qeye(3))
+
+
+    QND_ideal = qu.tensor(id_a, A0) + qu.tensor(X_a, A1)
+
+#   QND_ideal = 
 #     [[1. 0. 0. 0. 0. 0.]
 #      [0. 1. 0. 0. 0. 0.]
 #      [0. 0. 0. 0. 0. 1.]
 #      [0. 0. 0. 1. 0. 0.]
 #      [0. 0. 0. 0. 1. 0.]
 #      [0. 0. 1. 0. 0. 0.]]
-    
-# Define Omega state for computing Choi matrix 
-# omega = [00,01,02,10,11,12] * [00,01,02,10,11,12] ancilla first
-    omega = sum([qu.tensor(qu.basis(2, i), qu.basis(3, j), qu.basis(2, i), qu.basis(3, j)) 
-                    for i, j in product(range(2), range(3))])
-    dimHtot= 6
-    omega_state = omega * omega.dag()
-          
-#    QND_ideal =  qu.tensor(qu.tensor(qu.qeye(2),qu.qeye(3)), U_ideal) * omega_state * qu.tensor(qu.tensor(qu.qeye(2),qu.qeye(3)), U_ideal.dag())        
-    
 
-    p_dep = 1
-
-
-    choi_depolarizing = (p_dep * qu.tensor(qu.qeye(2), qu.qeye(3), qu.qeye(2) , qu.qeye(3))/dimHtot 
-                            + (1 - p_dep) * omega_state)
-
-    vals, vecs = eigh(choi_depolarizing.data.todense())
-
-    kraus_matrix = [np.sqrt(vals[j]) * vecs[:, j].reshape((dimHtot, dimHtot)).T for j in range(len(vecs))]
-#    print(sum([np.dot(np.conjugate(Op.T), Op) for Op in kraus_matrix]))    
-
-    system_length = len(choi_depolarizing.dims[0])//2
-    dim_kraus = [choi_depolarizing.dims[0][-system_length:], choi_depolarizing.dims[0][-system_length:]]   
-    kraus_ops = [qu.Qobj(inpt=op, dims=dim_kraus) for op in kraus_matrix]
-
-    channel_dep = sum([qu.spre(op) * qu.spost(op.dag()) for op in kraus_ops])
-    print(channel_dep)
-    print()
-
-    a = 1/np.sqrt(2)
-    b = 1/np.sqrt(2)
-#    a = 1
-#    b = 0
-    state_qutrit =qu.tensor(qu.basis(2,0),  (a * qu.basis(3,0) + b * qu.basis(3,1))).unit()
-    rho = state_qutrit * state_qutrit.dag()
-    print(channel_dep(state_qutrit))
-#    state_0 = (qu.tensor([state_qutrit, qu.basis(2,0)])).unit()
-
-
-#    print(    qu.process_fidelity(choi_ideal, choi_full_mixed))
-    
-    exit()
-
+    choi_experiment = np.genfromtxt("qubitqutrit_choi_noloss.csv", dtype=complex, delimiter=',')
 
     distances_list = []
     list_prob = np.linspace(0,1) #p of depolarizing noise
     
     for p_dep in list_prob:
-        # noisy channel
-        model_process = (1 - p_dep) * choi_ideal + p_dep * choi_full_mixed
+        channel_dep = depolarization_channel(p_dep)
+#construct the choi matrix of the ideal QND followed by the depolarization channel        
+        choi_parts_ideal_depol = []        
+        for i1, j1 in product(range(2), range(3)):
+            for i2, j2 in product(range(2), range(3)):    
+                state_ket = qu.tensor(qu.basis(2, i1), qu.basis(3, j1))
+                state_bra = qu.tensor(qu.basis(2, i2), qu.basis(3, j2)).dag()           
+                Qij = QND_ideal * state_ket * state_bra * QND_ideal.dag()
+                tenso_ij_depol_Qij = qu.tensor(state_ket * state_bra, channel_dep(Qij))
+                choi_parts_ideal_depol.append(tenso_ij_depol_Qij)
+        choi_ideal_and_depol = sum(choi_parts_ideal_depol) / 6.
 
-        row, cols = model_process.shape
-        u1 = model_process.full().reshape((row * cols, 1))
+        row, cols = choi_ideal_and_depol.shape
+        u1 = choi_ideal_and_depol.full().reshape((row * cols, 1))
         u2 = choi_experiment.reshape((row * cols, 1))
         distance_2 = scipy.linalg.norm(u1-u2)
         distances_list.append([p_dep, distance_2])
@@ -112,11 +108,16 @@ if __name__ == "__main__":
     arg_min = np.argmin(distances_array[:,1])
     print(distances_array[arg_min]) 
 
-    # pdepol_min, min_distance
-    # = [0.08163 0.31092]
 
     plt.plot(distances_array[:,0],distances_array[:,1], '-')
     plt.xlabel("depolarizing error p")
     plt.ylabel("cost function")    
-#    plt.savefig("distances_p_depol.pdf")
+    plt.title(f"depolarizing error p min = {distances_array[arg_min][0]:.3}")
+    ax = plt.gca()
+    from matplotlib.ticker import AutoMinorLocator
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+#    ax.tick_params(axis='x', which='minor', bottom=False)    
+    plt.plot([distances_array[arg_min][0]], [distances_array[arg_min][1]], 'o')
+    plt.savefig("distances_p_depol.pdf")
     plt.show()

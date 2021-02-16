e637f76711161cb24eb009fe7ae5baaf8eed25b6
Davide <davide.vodola@unibo.it>
2021-02-16 16:06:39 +0100

    Add precision in folder_name

--- simulation_all_over_stabilizers_depolarizing.py
+++ simulation_all_over_stabilizers_depolarizing.py
@@ -43,7 +43,7 @@ choi_ideal = np.loadtxt("choiFinal_ideal.dat")
 choi_experiment = np.genfromtxt("qubitqutrit_choi_noloss.csv", dtype=complex, delimiter=',')
 
 import os
-folder_name = f'chi_{chi_threshold:.01e}_eps_{epsilon_choi:1.3f}_over_stab'
+folder_name = f'chi_{chi_threshold:.01e}_eps_{epsilon_choi:1.3f}_depol_channel_p_{p_depolarizing_min:1.3f}'
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 

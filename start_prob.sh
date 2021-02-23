4f47124b1c947ee0cbf599d389799410b171f9a7
Davide Vodola <davide.vodola@unibo.it>
2021-02-23 10:35:51 +0100

    Add directory for data saving

--- start_prob.sh
+++ start_prob.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 export OMP_NUM_THREADS=1
     
-eps=0.90
+eps=0.0
 chi=0.0
-mkdir -p  $(printf "chi_%.01e_eps_%1.3f" $chi $eps )
+folder=$(printf "zzz_chi_%.01e_eps_%1.3f" $chi $eps )
+mkdir -p  $folder
 wait
 
-for state in 0 2 4 
+for state in 3
 do
-for c in 0.05  0.1  0.15  0.2  0.25  0.3  0.35  0.4  0.45  0.5  0.55  0.6  0.65  0.7  0.75  0.8  0.85  0.9  0.95
+for c in 0.1
 do
-        python process_matrix_simulation_all.py --logical_state ${state}  --phi_tilde ${c}  --epsilon_choi ${eps} --chi_threshold 0.0 2>&1 >& 0_log_${c}_${eps}_${state}.log & 
+        python simulation_all_over_stabilizers.py \
+         --logical_state ${state} \
+         --phi_tilde ${c} \
+         --epsilon_choi ${eps} \
+         --chi_threshold ${chi} \
+         --dir_name $folder
+           #2>&1 >& 0_log_${c}_${eps}_${state}.log & 
 done
 done
 
+
+
+#0.05  0.1  0.15  0.2  0.25  0.3  0.35  0.4  0.45  0.5  0.55  0.6  0.65  0.7  0.75  0.8  0.85  0.9  0.95

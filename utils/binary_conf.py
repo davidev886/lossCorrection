09732d2fe2146fc2226c61564bbbce2a406532e6
Davide Vodola <davide.vodola@unibo.it>
2021-02-24 21:10:42 +0100

    excluded configuration with 0 kept qubits

--- utils/binary_conf.py
+++ utils/binary_conf.py
@@ -2,19 +2,20 @@ import numpy as np
 
 from itertools import product
 
+
 class binary_raw_configurations(object):
-    def __init__(self, n = 6):
+    def __init__(self, n=6):
         self.n = n
         configurations = []
         for i in range(2**n):
             configuration_str = bin(i)[2:].zfill(n)
             configuration_int = [int(_) for _ in configuration_str]
             configurations.append(configuration_int)
-        self.configurations = configurations    
+        self.configurations = configurations
 
 
 class binary_configurations(object):
-    def __init__(self, n = 7):
+    def __init__(self, n=7):
         self.n = n
         configurations = {}
         for i in range(2**n):
@@ -24,27 +25,25 @@ class binary_configurations(object):
             if num_particles in configurations:
                 configurations[num_particles].append(configuration_int)
             else:
-                configurations[num_particles]= [configuration_int]
+                configurations[num_particles] = [configuration_int]
         self.configurations = configurations
-        
+
         conf_temp = []
-        a = []
         for j in range(n + 1):
             conf_temp.extend(configurations[j])
-        self.configurations_list = conf_temp        
-        
-            
+        self.configurations_list = conf_temp
+
     def generate_configuration_loss_qnderror(self, num_l, num_q):
         configuration_loss = []
         configuration_qnd = []
         configuration_loss = self.configurations[num_l]
-        configuration_qnd = self.configurations[num_q]   
-        
-        return product(configuration_loss, configuration_qnd)     
+        configuration_qnd = self.configurations[num_q]
+
+        return product(configuration_loss, configuration_qnd)
 
 
 class binary_configurations_loss_qnd_stab(object):
-    def __init__(self, n = 7, stab = 3 ):
+    def __init__(self, n=7, stab=3):
         self.n = n
         configurations = {}
         for i in range(2**n):
@@ -54,7 +53,7 @@ class binary_configurations_loss_qnd_stab(object):
             if num_particles in configurations:
                 configurations[num_particles].append(configuration_int)
             else:
-                configurations[num_particles]= [configuration_int]
+                configurations[num_particles] = [configuration_int]
         self.configurations = configurations
 
         configurations_stab = {}
@@ -65,24 +64,23 @@ class binary_configurations_loss_qnd_stab(object):
             if num_particles in configurations_stab:
                 configurations_stab[num_particles].append(configuration_int)
             else:
-                configurations_stab[num_particles]= [configuration_int]
+                configurations_stab[num_particles] = [configuration_int]
         self.configurations_stab = configurations_stab
-        
-        
-    
+
     def generate_configuration_loss_qnderror(self, num_l, num_q, num_stab):
         configuration_loss = []
         configuration_qnd = []
         configuration_loss = self.configurations[num_l]
-        configuration_qnd = self.configurations[num_q]   
-        configurations_stab = self.configurations_stab[num_stab]   
-    
-        return product(configuration_loss, configuration_qnd, configurations_stab)        
-        
-        
+        configuration_qnd = self.configurations[num_q]
+        configurations_stab = self.configurations_stab[num_stab]
+
+        return product(configuration_loss,
+                       configuration_qnd,
+                       configurations_stab)
+
 
 class binary_configurations_false_pn(object):
-    def __init__(self, n = 7, stab = 3 ):
+    def __init__(self, n=7, stab=3):
         self.n = n
         configurations = {}
         for i in range(2**n):
@@ -92,7 +90,7 @@ class binary_configurations_false_pn(object):
             if num_particles in configurations:
                 configurations[num_particles].append(configuration_int)
             else:
-                configurations[num_particles]= [configuration_int]
+                configurations[num_particles] = [configuration_int]
         self.configurations = configurations
 
         configurations_stab = {}
@@ -103,46 +101,51 @@ class binary_configurations_false_pn(object):
             if num_particles in configurations_stab:
                 configurations_stab[num_particles].append(configuration_int)
             else:
-                configurations_stab[num_particles]= [configuration_int]
+                configurations_stab[num_particles] = [configuration_int]
         self.configurations_stab = configurations_stab
-        
+
     def generate_configuration_loss_qnderror(self, num_l, num_q, num_stab):
         configuration_loss = []
         configuration_qnd = []
         configuration_loss = self.configurations[num_l]
-        configuration_qnd = self.configurations[num_q]   
-        configurations_stab = self.configurations_stab[num_stab]   
-    
-        return product(configuration_loss, configuration_qnd, configurations_stab) 
-        
-                
+        configuration_qnd = self.configurations[num_q]
+        configurations_stab = self.configurations_stab[num_stab]
+
+        return product(configuration_loss,
+                       configuration_qnd,
+                       configurations_stab)
+
 
 def check_correctable_state_analytics(random_losses, qnd_errors):
 
-    non_correctable_3_events = [[0, 1, 4], [0, 2, 5], [0, 3, 6], [1, 2, 6], [2, 3, 4], [4, 5, 6], [1, 3, 5]]
-    correctable_4_events = [[0, 1, 2, 3], [1, 2, 4, 5], [2, 3, 5, 6], [0, 3, 4, 5], [0, 1, 5, 6], [1, 3, 4, 6], [0, 2, 4, 6]]
+    non_correctable_3_events = [[0, 1, 4], [0, 2, 5], [0, 3, 6],
+                                [1, 2, 6], [2, 3, 4], [4, 5, 6], [1, 3, 5]]
+    correctable_4_events = [[0, 1, 2, 3], [1, 2, 4, 5],
+                            [2, 3, 5, 6], [0, 3, 4, 5],
+                            [0, 1, 5, 6], [1, 3, 4, 6],
+                            [0, 2, 4, 6]
+                            ]
 
-    num_loss = sum(random_losses)
-    num_qnd = sum(qnd_errors)  
-
-    guessed_loss = [(loss + qnd_err) % 2 for loss, qnd_err in zip(random_losses, qnd_errors)]
+    guessed_loss = [(loss + qnd_err) % 2
+                    for loss, qnd_err in zip(random_losses, qnd_errors)]
     num_fresh_qubits = sum(guessed_loss)
 
     position_loss = np.where(random_losses)[0].tolist()
     position_qnd = np.where(qnd_errors)[0].tolist()
-    
-    #find if a qnd_error hits a loss    
+
+    # find if a qnd_error hits a loss
     qnderror_hit_loss = any([(_ in position_loss) for _ in position_qnd])
-    
+
     if qnderror_hit_loss:
         non_correctable = True
         correctable = False
     else:
-        if  num_fresh_qubits in (0,1,2):
+        if num_fresh_qubits in (0, 1, 2):
             non_correctable = False
             correctable = True
         elif num_fresh_qubits == 3:
-            if sorted(position_loss + position_qnd) in non_correctable_3_events:
+            if sorted(position_loss + position_qnd) \
+               in non_correctable_3_events:
                 non_correctable = True
                 correctable = False
             else:
@@ -157,15 +160,13 @@ def check_correctable_state_analytics(random_losses, qnd_errors):
                 non_correctable = True
                 correctable = False
 
-        elif num_fresh_qubits in (5,6,7):
+        elif num_fresh_qubits in (5, 6, 7):
             non_correctable = True
             correctable = False
 
     return [correctable, non_correctable]
-    
-    
-    
-    
+
+
 def check_correctable_state(random_losses, qnd_errors):
     """
     Check whether a loss + qnd error event is correctable.
@@ -174,54 +175,53 @@ def check_correctable_state(random_losses, qnd_errors):
     or
     b. no qnd errors happens and 0,1,2 losses happen
     or
-    c. no qnd errors happens on the losses and the number of replaced qubits is 0,1,2
-    
-     
+    c. no qnd errors happens on the losses and the number
+       of replaced qubits is 0,1,2
+
     The cases that are NOT correctable with no doubts are the ones where
     d. one qnd error happens on the position of a loss
     or
-    e. five, six or seven fresh qubits are introduced for replacing the actual losses or the guessed ones
+    e. five, six or seven fresh qubits are introduced for replacing
+       the actual losses or the guessed ones
 
     In all the other cases, one should check
     """
-    
-    num_loss = sum(random_losses)
-    num_qnd = sum(qnd_errors)  
 
-    guessed_loss = [(loss + qnd_err) % 2 for loss, qnd_err in zip(random_losses, qnd_errors)]
+    guessed_loss = [(loss + qnd_err) % 2 for
+                    loss, qnd_err in zip(random_losses, qnd_errors)]
     num_fresh_qubits = sum(guessed_loss)
 
     position_loss = np.where(random_losses)[0].tolist()
     position_qnd = np.where(qnd_errors)[0].tolist()
-    
-    #find if a qnd_error hits a loss    
+
+    # find if a qnd_error hits a loss
     qnderror_hit_loss = any([(_ in position_loss) for _ in position_qnd])
-    
+
     if qnderror_hit_loss:
         non_correctable = True
         correctable = False
         to_check = False
     else:
-        if   num_fresh_qubits in (0,1,2):
+        if num_fresh_qubits in (0, 1, 2):
             non_correctable = False
             correctable = True
             to_check = False
-        elif num_fresh_qubits in (3,4):
+        elif num_fresh_qubits in (3, 4):
             non_correctable = False
             correctable = False
             to_check = True
 
-        elif num_fresh_qubits in (5,6,7):
+        elif num_fresh_qubits in (5, 6, 7):
             non_correctable = True
             correctable = False
             to_check = False
 
     return [correctable, non_correctable, to_check]
-    
+
 
 def get_ancilla_outcomes_false_negatives(L):
     all_binary_ordered_confs = []
-    for num_loss, loss_confs in binary_configurations().configurations.items():  
+    for num_loss, loss_confs in binary_configurations().configurations.items():
         all_binary_ordered_confs.extend(loss_confs)
 
     all_configurations = []
@@ -230,25 +230,36 @@ def get_ancilla_outcomes_false_negatives(L):
 
         false_negative_confs = []
         for false_neg_events_all in all_binary_ordered_confs:
-#            print("outcomes_ancilla", outcomes_ancilla, false_neg_events_all)
+            # print("outcomes_ancilla", outcomes_ancilla, false_neg_events_all)
             false_neg_events = [0] * L
-            for _ in range (L):
+            for _ in range(L):
                 if outcomes_ancilla[_] == 0:
                     false_neg_events[_] = false_neg_events_all[_]
                 else:
                     false_neg_events[_] = 0
 
-            if false_neg_events not in false_negative_confs:
+            losses = np.where(outcomes_ancilla)[0].tolist()
+            false_negative_qubits = np.where(false_neg_events)[0].tolist()
+            kept_qubits = [_ for _ in range(L)
+                           if (_ not in false_negative_qubits)
+                           and (_ not in losses)]
+            if false_neg_events not in false_negative_confs \
+               and \
+               len(kept_qubits):
                 false_negative_confs.append(false_neg_events)
 
         all_configurations.append([outcomes_ancilla, false_negative_confs])
-        index_outcomes_ancilla += 1               
+        index_outcomes_ancilla += 1
     return all_configurations
 
+
 if __name__ == "__main__":
-#     for _ in get_ancilla_outcomes_false_negatives(7):
-#         print(_)
-#         exit()
+    #     for _ in get_ancilla_outcomes_false_negatives(7):
+    #         print(_)
+    #         exit()
+    cc = 0
     a = get_ancilla_outcomes_false_negatives(7)
-    for ifd,[x,y] in enumerate(a):
-        print(ifd, x, "||", y)
+    for ifd, [x, y] in enumerate(a):
+        print(ifd, x, "||", len(y))
+        cc += len(y)
+    print(cc)

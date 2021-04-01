119367f0cbe11cdef9bcdc4416b409e7286bf5de
Davide Vodola <davide.vodola@unibo.it>
2021-04-01 23:28:57 +0200

    Script for case 2: incoherent effective channel

--- monte_carlo_incoherent.sh
+++ monte_carlo_incoherent.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 export OMP_NUM_THREADS=1
 
-dt=$(date '+%Y%m%d%H%M') 
+eps=0
+chi=0
 
-eps=0.0
-chi=0.0
-overrot_2=0.136
-overrot_1=0.01
-folder="$dt"_$(printf "mc_chi_%.01e_eps_%1.3f_p2_%1.3f_p1_%1.3f" $chi $eps ${overrot_2} ${overrot_1})
-echo $folder
+for overrot1 in 0 0.01 0.03162277660
+do
+for overrot2 in 0 0.136 0.4300697618
+do
 
+folder=$(printf "chi_incohe_%.01e_eps_%1.3f_p2_%1.3f_p1_%1.3f" $chi $eps ${overrot2} ${overrot1})
+echo ${overrot1}, ${overrot2}, ${folder}
 mkdir -p  $folder
-wait
-
-#done states 3
-for state in 0 #2
+i=0
+for state in 0 2 4
 do
-for c in $(seq -f "%1.2f" 0.05 0.05 0.95) 
+for c in $(seq -f "%1.2f" 0.05 0.05 0.95)
 do
-        python monte_carlo_simulation_false_positive_negative \ 
-         --logical_state ${state} \
-         --phi_tilde ${c} \
-         --epsilon_choi ${eps} \
-         --chi_threshold ${chi} \
-         --dir_name $folder \
-         --num_trials 10000 \
-         --p_overrot_2 ${overrot_2} \
-         --p_overrot_1 ${overrot_1} \
-           2>&1 >& 0_log_${c}_${eps}_${state}_${overrot_2}_${overrot_1}.log & 
+     echo $c
+     python monte_carlo_simulation_false_positive_negative.py \
+        --logical_state ${state}  \
+        --phi_tilde ${c}  \
+        --epsilon_choi ${eps} \
+        --chi_threshold ${chi} \
+        --num_trials 2000 \        
+        --p_overrot_1 ${overrot1} \
+        --p_overrot_2 ${overrot2} \
+        --dir_name $folder \
+        2>&1 >& 0_log_${c}_${eps}_${state}_${overrot2}_${overrot1}.log &
+    
+         pids[${i}]=$!
+         i=$((i+1))
 done
 done
 
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+done
+done

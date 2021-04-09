3f63af14675a0d4c8ab0428327573f8f1dabffc1
Davide <davide.vodola@unibo.it>
2021-04-09 09:16:45 +0200

    Add computation p_err_stab from p_overror_2

--- monte_carlo_fp_and_stab_error.py
+++ monte_carlo_fp_and_stab_error.py
@@ -72,6 +72,12 @@ folder_name = args.dir_name
 num_trials = args.num_trials
 p_err_stab = args.p_err_stab
 
+
+p1MS = (args.p_overrot_2 * np.pi / 2 / 2)**2
+p_err_stab = 4 * p1MS
+
+print(p_err_stab)
+
 choi_ideal = np.loadtxt("choi_op/choiFinal_ideal.dat")
 
 choi_experiment = np.genfromtxt("choi_op/qubitqutrit_choi_noloss.csv",

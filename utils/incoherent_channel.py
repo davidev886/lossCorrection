af94ee65a5a202b9ec812b0bda560dd1df39a05f
Davide <davide.vodola@unibo.it>
2021-05-19 12:19:59 +0200

    Add incoherent channel  for zero loss and qubits

---
+++ utils/incoherent_channel.py
@@ -0,0 +1,22 @@
+import numpy as np
+import qutip as qu
+from utils.p_operators import *
+
+def inc_channel(prob, n_qubit):
+    Xq = X[n_qubit]
+    channel = (prob['1a'] * qu.spre(Id) * qu.spost(Id) +
+               prob['1b'] * qu.spre(Xq) * qu.spost(Xq) +
+               prob['1c'] * qu.spre(Xq * Xa) * qu.spost(Xq * Xa) +
+               prob['1d'] * qu.spre(Xa) * qu.spost(Xa)
+               )
+    return channel
+
+if __name__ == "__main__":
+    print("c")
+    p = 1.00
+    qutrit_n = 6
+    vacuum = qu.tensor([qu.basis(3,0)] * L + [qu.basis(2,0)])
+
+    state_rho = vacuum*vacuum.dag()
+    for data_q in range(7):
+        state_rho = UnitaryQubitQutritQNDDepol(p, state_rho, data_q)

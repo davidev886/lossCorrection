712614cf47da30958543e249f0562a4c1ab3f3b1
Davide <davide.vodola@unibo.it>
2021-05-26 15:10:39 +0200

    Correct syntax

--- utils/incoherent_channel_qutrit.py
+++ utils/incoherent_channel_qutrit.py
@@ -2,6 +2,7 @@ import numpy as np
 import qutip as qu
 from utils.p_operators_qutrit import *
 
+
 def inc_channel(prob, n_qubit, noloss=True):
     Xq = X[n_qubit]
     if noloss:
@@ -13,24 +14,23 @@ def inc_channel(prob, n_qubit, noloss=True):
         return channel
     else:
         channel_0 = (prob['1a'] * qu.spre(Id) * qu.spost(Id) +
-                   prob['1b'] * qu.spre(Xq) * qu.spost(Xq) +
-                   prob['1c'] * qu.spre(Xq * Xa) * qu.spost(Xq * Xa) +
-                   prob['1d'] * qu.spre(Xa) * qu.spost(Xa)
-                   )
+                     prob['1b'] * qu.spre(Xq) * qu.spost(Xq) +
+                     prob['1c'] * qu.spre(Xq * Xa) * qu.spost(Xq * Xa) +
+                     prob['1d'] * qu.spre(Xa) * qu.spost(Xa)
+                     )
 
         channel_1 = (prob['2a'] * qu.spre(Id) * qu.spost(Id) +
-                   prob['2b'] * qu.spre(Xa) * qu.spost(Xa)
-                   )
+                     prob['2b'] * qu.spre(Xa) * qu.spost(Xa)
+                     )
 
         return channel_0, channel_1
-
 
 
 if __name__ == "__main__":
     print("c")
     p = 1.00
     qutrit_n = 6
-    vacuum = qu.tensor([qu.basis(3,0)] * L + [qu.basis(2,0)])
+    vacuum = qu.tensor([qu.basis(3, 0)] * L + [qu.basis(2, 0)])
 
     state_rho = vacuum*vacuum.dag()
     for data_q in range(7):

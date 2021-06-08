c33762c76b78b807cff1c36a473972399077a457
Davide <davide.vodola@unibo.it>
2021-06-08 17:58:40 +0200

    Add incoherent channel faster implementation

--- utils/incoherent_channel_qutrit.py
+++ utils/incoherent_channel_qutrit.py
@@ -3,35 +3,19 @@ import qutip as qu
 from utils.p_operators_qutrit import *
 
 
-def inc_channel(prob, n_qubit, noloss=True):
+def channel_E_0(rho_L, prob, n_qubit):
     Xq = X[n_qubit]
-    if noloss:
-        channel = (prob['1a'] * qu.spre(Id) * qu.spost(Id) +
-                   prob['1b'] * qu.spre(Xq) * qu.spost(Xq) +
-                   prob['1c'] * qu.spre(Xq * Xa) * qu.spost(Xq * Xa) +
-                   prob['1d'] * qu.spre(Xa) * qu.spost(Xa)
-                   )
-        return channel
-    else:
-        channel_0 = (prob['1a'] * qu.spre(Id) * qu.spost(Id) +
-                     prob['1b'] * qu.spre(Xq) * qu.spost(Xq) +
-                     prob['1c'] * qu.spre(Xq * Xa) * qu.spost(Xq * Xa) +
-                     prob['1d'] * qu.spre(Xa) * qu.spost(Xa)
-                     )
-
-        channel_1 = (prob['2a'] * qu.spre(Id) * qu.spost(Id) +
-                     prob['2b'] * qu.spre(Xa) * qu.spost(Xa)
-                     )
-
-        return channel_0, channel_1
+    rhof = (prob['1a'] * Id * rho_L * Id +
+            prob['1b'] * Xq * rho_L  * Xq +
+            prob['1c'] * Xq * Xa * rho_L * Xq * Xa +
+            prob['1d'] * Xa * rho_L * Xa
+            )
+    return rhof
 
 
-if __name__ == "__main__":
-    print("c")
-    p = 1.00
-    qutrit_n = 6
-    vacuum = qu.tensor([qu.basis(3, 0)] * L + [qu.basis(2, 0)])
-
-    state_rho = vacuum*vacuum.dag()
-    for data_q in range(7):
-        state_rho = UnitaryQubitQutritQNDDepol(p, state_rho, data_q)
+def channel_E_1(rho_L, prob, n_qubit):
+    Xq = X[n_qubit]
+    rhof = (prob['2a'] * Id * rho_L * Id +
+            prob['2b'] * Xa * rho_L * Xa
+            )
+    return rhof

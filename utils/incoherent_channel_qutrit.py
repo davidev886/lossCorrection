8901dc4662fb5e28dff3df9f57430d6d4192bfd3
Davide Vodola <davide.vodola@unibo.it>
2021-06-13 22:47:39 +0200

    Add projection on 01 qutrit states

--- utils/incoherent_channel_qutrit.py
+++ utils/incoherent_channel_qutrit.py
@@ -29,14 +29,18 @@ def new_channel(rho_L, prob, n_qubit):
                         + [proj(2, 2, dimH = 3)]
                         + [qu.qeye(dimHq)] * (L - n_qubit - 1)
                         + [qu.qeye(dimHa)])
+    P01 =  qu.tensor([qu.qeye(dimHq)] * n_qubit
+                      + [qu.qeye(dimHq) - proj(2, 2, dimH = 3)]
+                      + [qu.qeye(dimHq)] * (L - n_qubit - 1)
+                      + [qu.qeye(dimHa)])
     Xq = X[n_qubit]
 #     print(proj_2)
 #     print()
 #     print(rho_L)
-    rhof =  (prob['1a'] * Id * rho_L * Id +
-            prob['1b'] * Xq * rho_L  * Xq +
-            prob['1c'] * Xq * Xa * rho_L * Xq * Xa +
-            prob['1d'] * Xa * rho_L * Xa +
+    rhof =  (prob['1a'] * Id * P01 * rho_L * P01.dag() * Id +
+            prob['1b'] * Xq * P01 * rho_L * P01.dag()  * Xq +
+            prob['1c'] * Xq * Xa * P01 * rho_L * P01.dag() * Xq * Xa +
+            prob['1d'] * Xa * P01 * rho_L * P01.dag() * Xa +
             prob['2a'] * Xa * proj_2 *  rho_L * proj_2 * Xa +
             prob['2b'] * Xa * proj_2 *  rho_L * proj_2 * Xa
             )

cac36f8a96eb2a44c4ab2ec7bb64a6028f502b9c
Davide Vodola <davide.vodola@unibo.it>
2021-06-20 23:35:07 +0200

    add channel with no action on ancilla

--- utils/incoherent_channel_qutrit.py
+++ utils/incoherent_channel_qutrit.py
@@ -45,3 +45,27 @@ def new_channel(rho_L, prob, n_qubit):
             prob['2b'] * Xa * proj_2 *  rho_L * proj_2 * Xa
             )
     return rhof
+
+
+def new_channel_only_qutrit(rho_L, prob, n_qubit):
+    dimHa = 2
+    dimHq = 3
+
+    proj_2 = qu.tensor([qu.qeye(dimHq)] * n_qubit
+                        + [proj(2, 2, dimH = 3)]
+                        + [qu.qeye(dimHq)] * (L - n_qubit - 1)
+                        + [qu.qeye(dimHa)])
+    P01 =  qu.tensor([qu.qeye(dimHq)] * n_qubit
+                      + [qu.qeye(dimHq) - proj(2, 2, dimH = 3)]
+                      + [qu.qeye(dimHq)] * (L - n_qubit - 1)
+                      + [qu.qeye(dimHa)])
+    Xq = X[n_qubit]
+#     print(proj_2)
+#     print()
+#     print(rho_L)
+    rhof =  (prob['1a'] * Id * P01 * rho_L * P01.dag() * Id +
+            prob['1b'] * Xq * P01 * rho_L * P01.dag()  * Xq +
+            prob['1c'] * Xq  * P01 * rho_L * P01.dag() * Xq +
+            prob['1d'] * P01 * rho_L * P01.dag()
+            )
+    return rhof

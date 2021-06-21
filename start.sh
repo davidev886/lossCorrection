8996a3d861aa7e5e175307995c1d1c955457bbed
Davide Vodola <davide.vodola@unibo.it>
2021-06-21 23:58:14 +0200

    Start single overrotation

--- start.sh
+++ start.sh
@@ -11,18 +11,18 @@ chi=0
 
 for overrot1 in 0.010000 #$(seq -f "%1.5f" 0.000 0.00400 0.0141421)
 do
-for overrot2 in 0.020000 #$(seq -f "%1.5f" 0.000 0.0400 0.16) 0.136
+for overrot2 in 0.000000 #$(seq -f "%1.5f" 0.000 0.0400 0.16) 0.136
 do
 
-    folder=$(printf "case_2_zero_loss_commented/chi_%.01e_eps_%1.3f_p2_%1.5f_p1_%1.5f" $chi $eps ${overrot2} ${overrot1})
+    folder=$(printf "case_1_analytical/chi_%.01e_eps_%1.3f_p2_%1.5f_p1_%1.5f" $chi $eps ${overrot2} ${overrot1})
     echo ${overrot1}, ${overrot2}, ${folder}
     mkdir -p  $folder
     i=0
     c=0.
-    for state in 2
+    for state in 0
     do
     echo $overrot1 $overrot2  $state
-    python qec_incoherent_channel.py \
+    python  simulation_all_over_stabilizers_overrotation.py \
         --logical_state ${state}  \
         --phi_tilde ${c}  \
         --epsilon_choi ${eps} \
